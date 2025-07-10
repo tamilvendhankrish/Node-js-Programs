429df4ecac29a7a6461e63ef277fbc4c053ab483
Tamilvendhan <54322133+tamilvendhankrish@users.noreply.github.com>
2025-07-10 22:06:52 +0530

    Create express_pjt.js
    express app using express package

---
+++ express_pjt.js
@@ -0,0 +1,17 @@
+//add express package by C:user> npm install express --save
+//go to project folder
+//npm init 
+//run the node app
+
+//simple program using express & building an express app
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send('Works');});
+
+app.get('/test',(req,res)=>{
+    res.send('test page');});
+
+app.listen(port, ()=>{return console.log("ready and running");});
